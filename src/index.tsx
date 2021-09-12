9a8be12f73e7ceefc267fc69ad66217fa6ff61c7
diggitydingdong <mitchellinminecraft@gmail.com>
2021-09-13 09:55:11 +1000

    Installed classnames

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './document.css';
 import styles from './index.module.scss';
+import classNames from 'classnames';
 
+export const App: React.FC = () => {
+  const cn = classNames();
+  return <div className={cn}>
+
+  </div>
+}
+
+// Inject
 ReactDOM.render(
-  <></>,
+  <App/>,
   document.getElementById('root')
 );
